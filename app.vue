d05a676453960f1dfdb91e5ce5488917204cb5dd
Adrien Becker <adrien.be@helloasso.com>
2024-10-22 14:58:06 +0200

    dojo 22 oct: scope slot

--- app.vue
+++ app.vue
@@ -1,47 +1,45 @@
-<script set></script>
+<script setup>
+const tabs = [
+  {
+    name: "Tab1",
+    icon: "🔥",
+    content: "Content1",
+  },
+  { name: "Tab2", icon: "🔥", content: "Content2" },
+  { name: "Tab3", content: "Content3" },
+  { name: "Tab4", content: "Content4" },
+];
+
+const preventSelect = (index, callback) => {
+  if (confirm("sur ??")) 
+    callback(index);
+};
+</script>
+
 <template>
   <div>
-    
-<!-- test -->
-<Tabs>
-  <template>
-</Tabs>
+    <Tabs :tabs="tabs">
+      <!-- tab btn -->
+      <template #tab-1="{ tab, select, index }">
+        <button @click="preventSelect(index, select)">
+          <i>{{ tab.icon }}</i>
+          <i>{{ tab.name }}</i>
+        </button>
+      </template>
 
-
-<!-- 
-<Tabs :tabs="tabs" v-slot="{ currentTabContent }">
-  <button v-if="currentTabContent.cta">
-    Click
-  </button>
-  <h1>{{ title }}</h1>
-  <p>{{ content }}</p>
-  <component :is="HABadge" :title="title" />
-</Tabs>
--->
-
-
-<!-- 
-<Tabs>
-  
-  <template #tab_content title="name of tab 1">
-    <h1>{{ title }}</h1>
-    <p>{{ content }}</p>
-  </template>
-
-  <template #tab_content title="name of tab 2">
-    <h1>{{ title }}</h1>
-    fefe
-    <p>{{ content }}</p>
-  </template>
-
-  <template #tab_content title="name of tab 3">
-    <h1>{{ title }}</h1>
-    fefe
-    <p>{{ content }}</p>
-  </template>
-
-</Tabs>
--->
-
+      <!-- tab content -->
+      <template #tab-content="{ title, content, index }">
+        <div v-if="index == 1" :style="{ background: 'red' }">
+          coucou
+          <h3>{{ title }}</h3>
+          <p>{{ content }}</p>
+        </div>
+        <div v-else>
+          else
+          <h1>{{ title }}</h1>
+          <p>{{ content }}</p>
+        </div>
+      </template>
+    </Tabs>
   </div>
 </template>
--- components/Tabs.vue
+++ components/Tabs.vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   tabs: Array<{ name: string; content: string }>;
 }>();
+
+const selectedTabIndex = ref(0);
+const selectedTab = computed(() => props.tabs[selectedTabIndex.value]);
+
+const select = (index: number) => (selectedTabIndex.value = index);
 </script>
 
 <template>
-  <div class="tabs">
-    <button v-for="(tab, key) in tabs" :key="tab.name">
-      {{ tab.name }}
-    </button>
+  <div class="parent">
+    <div class="tabs">
+      <slot
+        v-for="(tab, index) in tabs"
+        :name="`tab-${index}`"
+        :tab="tab"
+        :index="index"
+        :select="select"
+      >
+        <button :key="tab.name" @click="select(index)">
+          {{ tab.name }}
+        </button>
+      </slot>
+    </div>
+    <div class="tabs-content">
+      <slot
+        name="tab-content"
+        :title="selectedTab.name"
+        :content="selectedTab.content"
+        :index="selectedTabIndex"
+      ></slot>
+    </div>
   </div>
-  <div v-for="tab in tabs" :key="tab.name" class="tabs-content" />
 </template>
